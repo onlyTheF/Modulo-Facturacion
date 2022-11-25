fadb1bbc105495cb2259f4409b444c6155f7d50b
axe1619 <89085938+axe1619@users.noreply.github.com>
2022-11-25 07:41:56 -0400

    mejoras en la app

--- facturacion/src/app/app-routing.module.ts
+++ facturacion/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalleFacturaComponent } from './DetalleFactura/DetalleFactura.component';
 import { GetComponent } from './Factura/get/get.component';
 
 const routes: Routes = [
   {path: 'get', component:GetComponent},
-  {path: '**', component:GetComponent}
+  {path: '**', component:GetComponent},
+  {path: 'detallefactura', component:DetalleFacturaComponent}
 ];
 
 @NgModule({
